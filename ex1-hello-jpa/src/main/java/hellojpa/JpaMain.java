ac4f346cfdd9d7f940e8a4458d8646082198c77a
ojiwon <ojw970725@gmail.com>
2024-12-30 18:49:44 +0900

    feature : 영속성 관리 - 비영속, 영속, 준영속 상태 구현

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = em.find(Member.class, 200L);
+            member.setName("AZAZ");
+
+            em.clear();
+            System.out.println("after clear == ");
+            Member member2 = em.find(Member.class, 200L);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
