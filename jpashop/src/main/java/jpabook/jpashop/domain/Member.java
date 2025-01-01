989a5b48b33fe0c3e509f7effc17b587fffccdeb
ojiwon <ojw970725@gmail.com>
2025-01-01 16:17:17 +0900

    feature : 실습2 - 연관관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,21 +1,24 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
 
     @Id
     @GeneratedValue
-    private Long id;
     @Column(name = "MEMBER_ID")
+    private Long id;
+
     private String name;
     private String city;
     private String street;
     private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
