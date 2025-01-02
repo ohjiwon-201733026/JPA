bc56e4648d31ad1906238ede98d8ec86b00e2a8f
ojiwon <ojw970725@gmail.com>
2025-01-02 22:29:55 +0900

    feature : 고급매핑 - MappedSuperclass - 셍성자, 생성일시, 수정자, 수정일시와 같은 공통으로 사양하는 정보 매핑하는 어노테이슨

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -17,16 +17,13 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie("바람과함께사라지다", 10000, "directorA", "actorA");
-            em.persist(movie);
-            em.flush();
-            em.clear();
+            Member member = new Member();
+            member.setCreatedBy("oh");
+            member.setCreatedDate(LocalDateTime.now());
+            member.setLastModifiedBy("kim");
+            member.setLastModifiedDate(LocalDateTime.now());
 
-            Item findMovie = em.find(Item.class, movie.getId());
-            System.out.println("findMovie: " + findMovie);
-
-
-
+            em.persist(member);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,11 +1,12 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue
