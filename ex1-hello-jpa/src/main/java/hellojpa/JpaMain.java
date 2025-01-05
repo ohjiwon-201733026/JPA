dac34d4d7b383a56fd0e1b61a9916e9f1bae1f50
ojiwon <ojw970725@gmail.com>
2025-01-05 16:21:07 +0900

    feature : 값타입 컬렉션

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,11 +18,36 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address1 = new Address("city", "state", "zip");
-            Address address2 = new Address("city", "state", "zip");
 
-            System.out.println("address1 == address2 : " + ( address1 == address2 ));
-            System.out.println("address1 equals address2 : " + ( address1.equals(address2) ));
+            Member member = new Member();
+            member.setName("member");
+            member.setHomeAddress(new Address("homeCity", "state", "zip"));
+
+            member.getFavoriteFoods().add("foodA");
+            member.getFavoriteFoods().add("foodB");
+            member.getFavoriteFoods().add("foodC");
+
+            member.getAddressHistory().add(new AddressEntity(new Address("city!", "state", "zip")));
+            member.getAddressHistory().add(new AddressEntity(new Address("city@", "state", "zip")));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println(" ========== ");
+
+            Member findMember = em.find(Member.class, member.getId());
+
+//            findMember.setHomeAddress(new Address("new-homeCity", "new-state", "new-zip")); // 값타입은 immutable해야함
+
+            // 컬렉션 수정
+//            findMember.getFavoriteFoods().remove("foodA");   // DELETE
+//            findMember.getFavoriteFoods().add("치킨");            // INSERT
+
+            System.out.println(" ADDRESS update !!");
+            findMember.getAddressHistory().remove(new AddressEntity(new Address("city!", "state", "zip"))); // 찾을 때 equals를 사용하므로 반드시 재정의해둬야함
+            findMember.getAddressHistory().add(new AddressEntity(new Address("newCity", "state", "zip"))); // INSERT
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,7 +2,10 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member extends BaseEntity{
@@ -21,13 +24,19 @@ public class Member extends BaseEntity{
     @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY"))
-            , @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET"))
-            , @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FODD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -59,5 +68,21 @@ public class Member extends BaseEntity{
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
