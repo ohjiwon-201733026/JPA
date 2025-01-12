013730e87e31f4a6eb2c4d33d149e326af9c914e
ojiwon <ojw970725@gmail.com>
2025-01-12 18:44:53 +0900

    feature : 연관관계 편의 메서드 추가, 지연로딩 설정, cascade 설정

--- jpashop2/src/main/java/jpabook/jpashop2/domain/Order.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,9 +32,19 @@ public class Order {
 
     private OrderStatus orderStatus;
 
-    public void changeMember(Member member) {
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 
 
