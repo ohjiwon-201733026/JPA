3279d3397c155d3c3834e7cd21fb5afbb750bea8
ojiwon <ojw970725@gmail.com>
2025-02-10 23:04:08 +0900

    feature : 회원 도메인 개발

---
+++ jpashop2/src/main/java/jpabook/jpashop2/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop2.service;
+
+import jpabook.jpashop2.domain.Member;
+import jpabook.jpashop2.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
